8c3d570789a21fea06ad5cf97b152cd7aefcc0d9
Bruno Ventura Gross <bvgross@gmx.com>
2025-06-29 00:46:13 -0300

    Teste de api do museu

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,41 +1,54 @@
 import { Image, Text, View } from "react-native";
-import { getMuseumObjectById, getMuseumObjectsByDepartmentId, listProps, objectProps } from '../../services/usuarios';
+import { getMuseumObjectById, getMuseumObjectsByDepartmentId, listProps, objectProps } from '../../services/museu';
 import { useEffect, useState } from 'react';
 import { styles } from './styles';
 
 export const Home = () => {
-  const [loading, setLoading] = useState(true);
-  const [objects, setObjects] = useState<listProps>({ total: 0, objectsIDs: [] });
-  const [object, setObject] = useState<objectProps>();
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getMuseumObjectsByDepartmentId(1);
-        setObjects(response.data);
 
-        const response2 = await getMuseumObjectById(45734);
-        setObject(response2.data);
+  //Escolher o departamento que quer consultar uma obra aleatória
+  const department = 5;
+
+  const [loading, setLoading] = useState(true);
+  const [list, setlist] = useState<listProps>({ total: 0, objectIDs: [] });
+  const [object, setObject] = useState<objectProps>();
+
+
+  //useEffect para baixar os ids das obras ao carregar a página
+  useEffect(() => {
+    const fetchList = async () => {
+      try {
+        const response = await getMuseumObjectsByDepartmentId(department);
+        setlist(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchList();
+  }, []);
+
+
+  //useEffect para pegar uma obra aleatória dessa lista carregada e exibir apenas as que tem imagem
+  useEffect(() => {
+    if (list.objectIDs.length === 0) return;
+
+    const fetchObject = async () => {
+      const randomObjectId = list.objectIDs[Math.floor(Math.random() * list.objectIDs.length)];
+      try {
+        let object;
+        do {
+          const response = await getMuseumObjectById(randomObjectId);
+          object = response.data;
+        } while (object.primaryImage === null); //só sai do loop se tiver imagem cadastrada
+
+        setObject(object);
       } catch (error) {
         console.log(error);
       } finally {
         setLoading(false);
       }
     };
-
-    fetchData();
-  }, []);
-
-  // return (
-  //   <View style={styles.container}>
-  //
-  //     <Text style={styles.temp} >{objects.total}</Text>
-  //     {object ? (
-  //       <Text>{object.title}</Text>
-  //     ) : (
-  //       <Text>Carregando objeto...</Text>
-  //     )}
-  //   </View >
-  // );
+    fetchObject();
+  }, [list]);
 
   if (loading) {
     return (
@@ -43,14 +56,15 @@ export const Home = () => {
         <Text>Carregando dados do museu...</Text>
       </View>
     );
-  }
-
-  return (
+  } return (
     <View style={styles.container}>
-      <Text style={styles.temp}>{objects.total}</Text>
-      <Text>{object?.culture}</Text>
-      <Text>{object?.artistDisplayName}</Text>
-      <Image src={object?.primaryImage} />
+      <Text style={styles.temp}>Total de objetos do departamento "{object?.department}" é: {list.total}</Text>
+
+      <Text>Id do objeto: {object?.objectID}</Text>
+      <Text>Cultura mãe do objeto: {object?.culture}</Text>
+      <Text>Período: {object?.period ? object?.period : "Periodo desconhecido"}</Text>
+      <Text>Nome do artista: {object?.artistDisplayName ? object?.artistDisplayName : "Artista desconhecido"}</Text>
+      <Image source={{ uri: object?.primaryImage }} style={{ width: 200, height: 200 }} />
     </View>
   );
 };
