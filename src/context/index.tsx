23a47648ee6b08e38e0da4617f06b8304a0db78c
José Carlos Boaretto Netto <boarettonetto@gmail.com>
2025-07-01 19:38:07 -0300

    adicionando o object 2 para fazermos o quiz

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,30 +1,32 @@
-import { createContext, useEffect, useState } from 'react';
-import { getMuseumObjectById, getMuseumObjectsByDepartmentId, listProps, objectProps } from '../services/museu';
+import { createContext, useEffect, useState } from "react";
+import { getMuseumObjectById, getMuseumObjectsByDepartmentId, listProps, objectProps } from "../services/museu";
 
 interface GameProviderProps {
   children: React.ReactNode;
 }
 
 interface GameContextType {
-  list: listProps,
-  setMuseumList: (department: number) => Promise<void>,
-  object: objectProps | undefined,
+  list: listProps;
+  setMuseumList: (department: number) => Promise<void>;
+  object: objectProps | undefined;
+  object2: objectProps | undefined;
   setMuseumObject: (list: listProps) => Promise<void>;
   loading: boolean;
 }
 
 export const GameContext = createContext<GameContextType>({
   list: { total: 0, objectIDs: [] },
-  setMuseumList: async () => { },
+  setMuseumList: async () => {},
   object: undefined,
-  setMuseumObject: async () => { },
+  object2: undefined,
+  setMuseumObject: async () => {},
   loading: true,
 });
 
 export const GameProvider = ({ children }: GameProviderProps) => {
-
   const [list, setlist] = useState<listProps>({ total: 0, objectIDs: [] });
   const [object, setObject] = useState<objectProps>();
+  const [object2, setObject2] = useState<objectProps>();
   const [loading, setLoading] = useState<boolean>(true);
 
   //baixar os ids das obas ao carregar a página
@@ -33,7 +35,7 @@ export const GameProvider = ({ children }: GameProviderProps) => {
     try {
       const response = await getMuseumObjectsByDepartmentId(department);
       setlist(response.data);
-      setMuseumObject(response.data)
+      setMuseumObject(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -42,24 +44,40 @@ export const GameProvider = ({ children }: GameProviderProps) => {
   //baixar um objeto aleatório da lista
   const setMuseumObject = async (list: listProps) => {
     if (list.objectIDs.length === 0) return;
-    let object;
-    let tentativas = 0;
-    const tentativasMaximas = 10;
-    do {
-      const randomObjectId = list.objectIDs[Math.floor(Math.random() * list.objectIDs.length)];
-      try {
-        const response = await getMuseumObjectById(randomObjectId);
-        object = response.data;
-        tentativas++;
-      } catch (error) {
-        console.log(error);
-        tentativas++;
-      }
-    } while (!object?.primaryImageSmall && tentativas < tentativasMaximas);
+    const getRandonObject = async () => {
+      let object;
+      let tentativas = 0;
+      const tentativasMaximas = 10;
+      do {
+        const randomObjectId = list.objectIDs[Math.floor(Math.random() * list.objectIDs.length)];
+        try {
+          const response = await getMuseumObjectById(randomObjectId);
+          object = response.data;
+          tentativas++;
+        } catch (error) {
+          console.log(error);
+          tentativas++;
+        }
+      } while (
+        !object?.primaryImageSmall ||
+        !object?.culture?.trim() ||
+        !object?.artistDisplayName?.trim() ||
+        !object?.period?.trim() ||
+        tentativas < tentativasMaximas
+      );
+      return object;
+    };
+    const object = await getRandonObject();
+    const object2 = await getRandonObject();
+    console.log(object);
+    console.log(object2);
 
-    if (object?.primaryImageSmall) {
+    if (object?.primaryImageSmall && object2?.primaryImageSmall) {
       console.log("setou um objeto");
       setObject(object);
+      setObject2(object2);
+      console.log("funcionou");
+
       setLoading(false);
     } else {
       console.log("Não foi possível encontrar um objeto com imagem");
@@ -72,6 +90,7 @@ export const GameProvider = ({ children }: GameProviderProps) => {
         list,
         setMuseumList,
         object,
+        object2,
         setMuseumObject,
         loading,
       }}
@@ -79,4 +98,4 @@ export const GameProvider = ({ children }: GameProviderProps) => {
       {children}
     </GameContext.Provider>
   );
-}
+};
