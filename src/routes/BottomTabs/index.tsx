4554c261dc6a74a599d67293a9c24186308e7c76
marlosbianna <marlos.bianna@aluno.serratec.org>
2025-07-01 12:18:29 -0300

    Icones do Bottom tabs

--- src/routes/BottomTabs/index.tsx
+++ src/routes/BottomTabs/index.tsx
@@ -2,18 +2,24 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image } from 'react-native';
 import CartIcon from '../../assets/iconesNavigator/CartIcon.png';
 import ShopIcon from '../../assets/iconesNavigator/CartIcon.png';
-import SkillIcon from '../../assets/iconesNavigator/CartIcon.png';
+import GameIcon from '../../assets/iconesNavigator/GameIcon.png';
 import leaderboardIcon from '../../assets/iconesNavigator/leaderboardIcon.png';
-import { Login } from '../../screens/login';
-import { Cadastro } from '../../screens/cadastro';
+import ProfileIcon from '../../assets/iconesNavigator/ProfileIcon.png';
+import AboutIcon from '../../assets/iconesNavigator/AboutIcon.png';
+import RulesIcon from '../../assets/iconesNavigator/RulesIcon.png';
 import { Home } from '../../screens/home';
 import { Leaderboard } from '../../screens/leaderboard';
+import { About } from '../../screens/about';
+import { Profile } from '../../screens/profile';
+import { Rules } from '../../screens/rules';
+
 
 const Tab = createBottomTabNavigator<BottomTabsParamList>();
 
 export type BottomTabsParamList = {
-  Login: undefined;
-  Cadastro: undefined;
+  About: undefined;
+  Profile: undefined;
+  Rules: undefined;
   Home: undefined;
   Leaderboard: undefined;
 };
@@ -36,7 +42,7 @@ export function BottomTabs() {
             <Image
               resizeMode='contain'
               style={{ width: 30, tintColor: color }}
-              source={CartIcon}
+              source={GameIcon}
             />
           )
         }}
@@ -55,14 +61,40 @@ export function BottomTabs() {
         }}
       />
       <Tab.Screen
-        name="Cadastro"
-        component={Cadastro}
+        name="Profile"
+        component={Profile}
         options={{
           tabBarIcon: ({ color }) => (
             <Image
               resizeMode='contain'
               style={{ width: 30, tintColor: color }}
-              source={ShopIcon}
+              source={ProfileIcon}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Rules"
+        component={Rules}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              resizeMode='contain'
+              style={{ width: 30, tintColor: color }}
+              source={RulesIcon}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="About"
+        component={About}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              resizeMode='contain'
+              style={{ width: 30, tintColor: color }}
+              source={AboutIcon}
             />
           )
         }}
---
+++ src/screens/about/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+export const About = () => {
+  return (
+    <Text>
+      Pagina About
+    </Text>
+
+  );
+};
